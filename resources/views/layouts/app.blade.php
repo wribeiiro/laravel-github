cbc80641db6affc752a047e4a97a63db7ffb13b9
Wellisson Ribeiro <welleh10@gmail.com>
2021-11-20 12:01:20 -0300

    remove jquery slim

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -100,7 +100,6 @@
         </main>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://getbootstrap.com/docs/4.0/assets/js/vendor/popper.min.js"></script>
     <script src="https://getbootstrap.com/docs/4.0/dist/js/bootstrap.min.js"></script>
 </body>
