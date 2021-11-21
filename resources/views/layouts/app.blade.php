24337bc10685733189068780e6465fa0be55a91b
Wellisson Ribeiro <welleh10@gmail.com>
2021-11-21 14:45:51 -0300

    :bug: fix burg menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,9 @@
                     {{ config('app.name', 'Devs of Tomorrow') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
+                    <span class="navbar-toggler-icon">
+                        <i class="fa fa-bars fa-2x"></i>
+                    </span>
                 </button>
 
                 <div class="collapse navbar-collapse text-white" id="navbarSupportedContent">
